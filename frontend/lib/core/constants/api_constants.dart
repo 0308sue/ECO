87e4624841ea62266637e7c701710d75c3f2bf29
0308sue <b0107701@naver.com>
2026-06-10 01:29:19 +0900

    랭킹 수정

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -26,12 +26,14 @@ String get receiptSaveUrl {
   return '$authApiBaseUrl/api/receipts';
 }
 
-
-
 String get recommendationItemsUrl {
   return '$authApiBaseUrl/api/recommendations/items';
 }
 
 String get recommendationPlacesUrl {
   return '$authApiBaseUrl/api/recommendations/places';
+}
+
+String get rankingUrl {
+  return '$authApiBaseUrl/api/rankings';
 }
